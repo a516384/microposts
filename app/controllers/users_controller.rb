1f61056f9d57b132a1d47107c488a0ce257e1980
a516384 <a0309mcdnom@yahoo.co.jp>
2015-08-25 14:36:05 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :destroy]
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -7,7 +7,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def edit
+   @user = User.find(params[:id])
+  end
+ 
+  def update
+   @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "update ok!"
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end
+ 
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,7 +35,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :local, :profile, :password,
                                  :password_confirmation)
+  end
+  def set_user
+    @message = User.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816122840) do
+ActiveRecord::Schema.define(version: 20150825122205) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "profile"
+    t.string   "local"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
